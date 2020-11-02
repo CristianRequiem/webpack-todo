a7c5bd5a20274c41ddaffc413fc892fe85dd20fe
Cristian <cristianrequiem@yahoo.es>
2020-11-02 14:58:53 -0600

    index.js actualizado

--- src/index.js
+++ src/index.js
@@ -5,17 +5,3 @@ import { crearTodoHtml } from './js/componentes';
 export const todoList = new TodoList();
 
 todoList.todos.forEach( todo => crearTodoHtml( todo ) );
-// todoList.todos[0].imprimirClase();
-// todoList.todos.forEach( crearTodoHtml ); Es lo mismo que la línea anterior, pero resumido
-
-// const tarea = new Todo('Aprender Javascript');
-// todoList.nuevoTodo(tarea);
-// console.log(todoList);
-// crearTodoHtml( tarea );
-
-// localStorage.setItem('mi-key','ABC1234');
-// sessionStorage.setItem('mi-key', 'ABC1234');
-
-// setTimeout(()=> {
-//     localStorage.removeItem('mi-key');
-// }, 1500);
